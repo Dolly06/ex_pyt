3813d2327cda1b096931c8ad62d442067d68fddc
Tendonge Dolly <tendongedolly@gmail.com>
2021-05-29 15:37:57 -0400

    Calculates Salary

---
+++ ex1/ex1.py
@@ -0,0 +1,9 @@
+hours = input('Enter number of hours: ')
+rate = input('Enter Rate: ')
+pay = int(hours) * int(rate)
+print('Pay:', pay)
+a = 1 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9
+print(a)
+
