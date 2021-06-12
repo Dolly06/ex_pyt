762172669c554f51d35a798dc1909038417f4b9f
Tendonge Dolly <tendongedolly@gmail.com>
2021-06-12 02:16:11 -0400

    Simple exercise on tuples

---
+++ ex8/ex8_01.py
@@ -0,0 +1,12 @@
+d = dict()  # creates dictionary
+d['csev'] = 1  # assigns value to key
+d['cwen'] = 2
+for (k,v) in d.items():  # loops through dictionary items and stores in a tuple
+    print(k,v)  # prints items
+tups = d.items()  # assigns tuple to tups
+print(tups)  # prints tuple
+'''l = list()
+print(type(l))
+l.append(2)
+l.insert(0,5)
+print(l)'''
